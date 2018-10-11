ed15136bc89b634c446a1ebb11ff3904786d2cd8
Uy Du TRAN <tranuydu@gmail.com>
2018-10-11 22:01:07 +0200

    Integrate Jest Unit Testing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,10 +7,6 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   email: string;
   password: string;
-
-  constructor(public navCtrl: NavController) {
-
-  }
 
   login() {
       console.log(this.email + '' + this.password);
